9e75bd763e950bd0b177433e891aa4b72d564df2
David Katz <15Dkatz@shcp.edu>
2018-04-15 17:30:18 -0700

    Add /callback Route

--- authentication/src/index.js
+++ authentication/src/index.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import { render } from 'react-dom';
 import history from './history';
-import { Router, Switch, Route } from 'react-router-dom';
+import { Router, Switch, Route, Redirect } from 'react-router-dom';
 import { App, Ramen, Sushi } from './components';
 import Auth from './Auth';
 
 const auth = new Auth();
 
+const callbackComponent = props => {
+  if (props.location.hash.includes('access_token')) {
+    return <h4>loading...</h4>;
+  } else {
+    return <Redirect to={{ pathname: '/' }} />
+  }
+};
+
 render(
   <Router history={history}>
     <Switch>
       <Route exact path='/' render={() => <App auth={auth} />} />
+      <Route path='/callback' render={props => callbackComponent(props)} />
       <Route path='/ramen' component={Ramen} />
       <Route path='/sushi' component={Sushi} />
     </Switch>
